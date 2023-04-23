471eb4e66583dc0c69d365ffe2ec2367b3cd416f
Mike <miketandy@protonmail.com>
2023-04-23 18:48:56 +0100

    refactor lightbox gallery

--- src/components/common/LightboxGallery/LightboxGallery.tsx
+++ src/components/common/LightboxGallery/LightboxGallery.tsx
@@ -1,15 +1,6 @@
 import { LegacyRef } from "react"
 import { Gallery, Item } from "react-photoswipe-gallery"
-import img2 from "../../../assets/gallery/2.jpg"
-import img4 from "../../../assets/gallery/4.jpg"
-import img7 from "../../../assets/gallery/7.jpg"
-import img14 from "../../../assets/gallery/14.jpg"
-import img15 from "../../../assets/gallery/15.jpg"
-import img18 from "../../../assets/gallery/18.jpg"
-import img19 from "../../../assets/gallery/19.jpg"
-import img20 from "../../../assets/gallery/20.jpg"
-import img22 from "../../../assets/gallery/22.jpg"
-import img23 from "../../../assets/gallery/23.jpg"
+import { photos } from "../../../data/gallery"
 import "photoswipe/dist/photoswipe.css"
 import styles from "./LightboxGallery.module.css"
 
@@ -20,142 +11,31 @@ export const LightboxGallery = () => {
     <div className={styles.root}>
       <Gallery withCaption>
         <div className={styles.itemsContainer}>
-          <div className={styles.item}>
-            <Item
-              original={img2}
-              thumbnail={img2}
-              width='513'
-              height='683'
-              caption='ss'
-            >
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img2}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img4} thumbnail={img4} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img4}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img7} thumbnail={img7} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img7}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img14} thumbnail={img14} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img14}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img15} thumbnail={img15} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img15}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img18} thumbnail={img18} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img18}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img19} thumbnail={img19} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img19}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img20} thumbnail={img20} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img20}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img22} thumbnail={img22} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img22}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
-          <div className={styles.item}>
-            <Item original={img23} thumbnail={img23} width='513' height='683'>
-              {({ ref, open }) => (
-                <img
-                  ref={ref as ImgRef}
-                  className={styles.image}
-                  onClick={open}
-                  src={img23}
-                  alt='Michael Tandy - Software Engineer'
-                />
-              )}
-            </Item>
-          </div>
+          {photos.map((photo, index) => {
+            const { imgSrc, caption, altText } = photo
+
+            return (
+              <div className={styles.item} key={index}>
+                <Item
+                  original={imgSrc}
+                  thumbnail={imgSrc}
+                  width='513'
+                  height='683'
+                  caption={caption ? caption : ""}
+                >
+                  {({ ref, open }) => (
+                    <img
+                      ref={ref as ImgRef}
+                      className={styles.image}
+                      onClick={open}
+                      src={imgSrc}
+                      alt={altText}
+                    />
+                  )}
+                </Item>
+              </div>
+            )
+          })}
         </div>
       </Gallery>
     </div>
---
+++ src/data/gallery/gallery.ts
@@ -0,0 +1,69 @@
+import img2 from "../../assets/gallery/2.jpg"
+import img4 from "../../assets/gallery/4.jpg"
+import img7 from "../../assets/gallery/7.jpg"
+import img14 from "../../assets/gallery/14.jpg"
+import img15 from "../../assets/gallery/15.jpg"
+import img18 from "../../assets/gallery/18.jpg"
+import img19 from "../../assets/gallery/19.jpg"
+import img20 from "../../assets/gallery/20.jpg"
+import img22 from "../../assets/gallery/22.jpg"
+import img23 from "../../assets/gallery/23.jpg"
+
+interface Photo {
+  imgSrc: string
+  caption?: string
+  altText: string
+}
+
+export const photos: Photo[] = [
+  {
+    imgSrc: img2,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img4,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img7,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img14,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img15,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img18,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img19,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img20,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img22,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+  {
+    imgSrc: img23,
+    caption: "caption",
+    altText: "Michael Tandy - Software Engineer",
+  },
+]
