141f3d4a27c767ff36c964a16fe524e166785a6e
Mike <miketandy@protonmail.com>
2023-04-30 10:59:20 +0100

    add 404 page

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,5 +1,22 @@
+import { Container } from "../../common/Container"
+import { HyperLink } from "../../common/HyperLink/HyperLink"
+import { Layout } from "../../common/Layout"
 import styles from "./NotFound.module.css"
 
 export const NotFound = () => {
-  return <div className={styles.root}>NotFound page</div>
+  return (
+    <Layout>
+      <div className={styles.root}>
+        <Container>
+          <h1>404 Not Found</h1>
+          <p>Page does not exist</p>
+          <p>
+            <HyperLink to='/' internalLink arrow>
+              return home
+            </HyperLink>
+          </p>
+        </Container>
+      </div>
+    </Layout>
+  )
 }
