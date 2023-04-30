a91a4e9dd65484194015f7b1261bfae50852d856
Mike <miketandy@protonmail.com>
2023-04-30 12:03:17 +0100

    update cv

--- src/data/jobs/jobs.ts
+++ src/data/jobs/jobs.ts
@@ -3,44 +3,42 @@ import { Job } from "../../types/Job"
 export const jobs: Job[] = [
   {
     id: "0b4c05b4-15c8-446f-b8b1-44af30f142b2",
-    title: "Software Engineer",
-    company: "Company Industries Ltd",
-    dates: "01/01/2001 - 01/01/2002",
+    title: "Frontend Software Engineer (React)",
+    company: "Avayler / Halfords",
+    dates: "02/2023 - Present",
     overview:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Developing FE features for various modules of the Avayler platform.",
     technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "React, TypeScript, Styled Components, React Query, TRPC, Ant Design, Material UI, Jest, Testing Library, Azure Devops, Scrumban, ESLint",
     responsibilities:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
-    achievements:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Agile ceremonies, product backlog refinement, peer reviews, estimates, delivering robust features and fixing bugs.",
+    achievements: "Delivering of robust new features.",
   },
   {
     id: "1928ce9e-7a87-48d3-8e58-3e3c768ed2f1",
-    title: "Software Engineer",
-    company: "Company Industries Ltd",
-    dates: "01/01/2001 - 01/01/2002",
-    overview:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+    title: "Frontend Software Engineer (React)",
+    company: "Ori",
+    dates: "08/2022 - 12/2022",
+    overview: "Developing the frontend for Ori Global Cloud.",
     technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "React, TypeScript, CSS modules, Formik, React Query, Jest, Testing Library, Cypress, Storybook, Figma, Git / Version Control, Scrum, Jira.",
     responsibilities:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Agile ceremonies, reviewing PRs, presentation / demo of new FE features to engineering team at the end of sprint.",
     achievements:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Production of robust, clean, peer reviewed code covered by automated tests. Timely delivery and presentation of features.",
   },
   {
     id: "7c30b68f-2e9d-4cb6-b1a8-1d07ecf0e68f",
-    title: "Software Engineer",
-    company: "Company Industries Ltd",
-    dates: "01/01/2001 - 01/01/2002",
+    title: "Frontend Developer",
+    company: "Prototype Creative",
+    dates: "03/2021 - 07/2022",
     overview:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Working on various projects for a web agency; a mix of corporate websites, online stores, and web applications.",
     technologies:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "HTML, CSS, Sass, BEM, JavaScript, React, Vue, jQuery, Bootstrap, Tailwind.",
     responsibilities:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Building projects from designs, estimating tasks, writing user stories, communication with project management team. Building new features and fixing bugs.",
     achievements:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi, amet, commodi repudiandae fugiat animi cupiditate impedit optio molestiae maxime omnis voluptatum corporis error nam dignissimos aliquid eos voluptas reiciendis.",
+      "Developed and launched projects on time and within budget. Maintained consistent design patterns and code standards across all projects.",
   },
 ]
